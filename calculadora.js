dbfa608f7e5b998db90b7acaf7ad60e98b343cac
phpablo <pablohenrique120@gmail.com>
2024-02-27 16:49:19 -0300

    :tada: primeiro commit do projeto - inclui os arquivos iniciais planejados

---
+++ calculadora.js
@@ -0,0 +1,37 @@
+// cria a classe da calculadora 
+class Calculadora {
+  
+  // funcão para somar 
+  somar(num1, num2) {
+    return num1 + num2;
+  }
+
+  // funcao para subtrair 
+  subtrair(num1,num2) {
+    return num1 - num2;
+  }
+
+  // funcao para multiplicar  
+  multiplicar (num1,num2) {
+    return num1 * num2;
+  }
+
+  // funcao para dividir
+  dividir (num1,num2) {
+    return num1 / num2;
+  }
+
+
+  // funcao da potência 
+  potencia (num1,num2) {
+    return Math.pow(num1,num2);
+  }
+
+  // funcao de raizQuadrada 
+  raizQuadrada (num1,num2) {
+    return Math.sqrt(num1,num2);
+  }
+
+}
+
+
---
+++ index.js
@@ -0,0 +1,8 @@
+var readlineSync = require('readline-sync');
+var nome = readlineSync.question('Qual seu nome ? ');
+console.log('Você digitou:' + nome);
+
+
+// Não funciona
+var nome = prompt('Qual seu nome ?');
+console.log(`Olá ${nome}`);
